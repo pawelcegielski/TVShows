064e801880f9610243ef440d831f641eabadecd8
Pawel Cegielski <mypood@gmail.com>
2020-05-13 22:20:34 +0200

    initilal commit

---
+++ TVShows/TVShows/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TVShows
+//
+//  Created by Pawel Cegielski on 13/05/2020.
+//  Copyright © 2020 Pawel Cegielski. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TVShows/TVShows/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TVShows
+//
+//  Created by Pawel Cegielski on 13/05/2020.
+//  Copyright © 2020 Pawel Cegielski. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
